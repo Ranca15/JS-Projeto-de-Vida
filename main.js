ea3107147e55db932db7ed2efaeda3e69737bfd1
Ranca15 <145451866+Ranca15@users.noreply.github.com>
2025-04-16 08:14:16 -0300

    2

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-10-05T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
