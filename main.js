46fc1536c89f33320968e4f9925af786fc719690
Ranca15 <145451866+Ranca15@users.noreply.github.com>
2025-05-07 11:12:30 -0300

    3

--- main.js
+++ main.js
@@ -41,11 +41,13 @@ function calculaTempo(tempoObjetivo) {
     }
 }
 
-function atualizaCronometro() {
-    for (let i = 0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
-    }
-}
+
+    function atualizaCronometro() {
+        for (let i = 0; i < contadores.length; i++) {
+          //contadores[i].textContent = calculaTempo(tempos[i]);
+        }
+      }
+
 
 function comecaCronometro() {
     atualizaCronometro();
